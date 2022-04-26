732ffbc9f37b448a4dc3643e10fb1563db1d5c41
unknown <marcelobettini@yahoo.com.ar>
2022-04-26 16:34:19 -0300

    recuperamos async/await en los modelos

--- Clase10/users/usersModel.js
+++ Clase10/users/usersModel.js
@@ -1,59 +1,59 @@
 const pool = require("../db/config");
 
-const getAllUsers = () => {
+const getAllUsers = async () => {
     const query = "SELECT * FROM users"
     try {
-        return pool.query(query)
+        return await pool.query(query)
     } catch (error) {
         error.message = error.code
         return error
     }
 }
 
-const getUserById = (id) => {
+const getUserById = async (id) => {
     const query = `SELECT * FROM users WHERE id = ${id}`
     try {
-        return pool.query(query)
+        return await pool.query(query)
     } catch (error) {
         error.message = error.code
         return error
     }
 }
 
-const addNewUser = (user) => {
+const addNewUser = async (user) => {
     const query = `INSERT INTO users SET ?`
     try {
-        return pool.query(query, user)
+        return await pool.query(query, user)
     } catch (error) {
         error.message = error.code
         return error
     }
 }
 
-const loginUser = (email) => {
+const loginUser = async (email) => {
     const query = `SELECT * FROM users WHERE email = '${email}'`
     try {
-        return pool.query(query)
+        return await pool.query(query)
     } catch (error) {
         error.message = error.code
         return error
     }
 }
 
-const deleteUserById = (id) => {
+const deleteUserById = async (id) => {
     const query = `DELETE FROM users WHERE id = ${id}`
     try {
-        return pool.query(query)
+        return await pool.query(query)
     } catch (error) {
         error.message = error.code
         return error
     }
 }
 
-const editUserById = (id, user) => {
+const editUserById = async (id, user) => {
     const query = `UPDATE users SET ? WHERE id = ${id}`
     try {
-        return pool.query(query, user)
+        return await pool.query(query, user)
     } catch (error) {
         error.message = error.code
         return error
