9c3199a9b8bd93dc706d6cd0d2caf591b5353f12
unknown <marcelobettini@yahoo.com.ar>
2022-05-03 22:10:40 -0300

    Clase 14 + conditional rendering

--- Clase14-React/efectos/src/App.jsx
+++ Clase14-React/efectos/src/App.jsx
@@ -12,11 +12,17 @@ function App() {
   }, [endpoint])
   return (
     <>
-      <h3>texto</h3>
-      {data.map((el) => <p key={el.id}>{el.id}</p>)}
+      {/* elaborar un render condicional, de acuerdo al tipo de recurso que llega desde la API (recuerden, posts, users y comments no comparten las mismas propiedades, a excepción del id. Entonces, de acuerdo al endpoint será el render)
+      Si no pueden utilizar un if de forma directa en el código jsx, pueden hacerlo por vía oblicua con un operador lógico...
+      😎 */}
+      <h3>Botones</h3>
       <button onClick={() => setEndpoint("posts")}>posts</button>
       <button onClick={() => setEndpoint("users")}>users</button>
       <button onClick={() => setEndpoint("comments")}>comments</button>
+      <h3>Resultado</h3>
+      {endpoint === "posts" && data.map((el) => <p key={el.id}>{el.title}</p>)}
+      {endpoint === "comments" && data.map((el) => <p key={el.id}>{el.name}</p>)}
+      {endpoint === "users" && data.map((el) => <p key={el.id}>{el.email}</p>)}
     </>
   )
 }
