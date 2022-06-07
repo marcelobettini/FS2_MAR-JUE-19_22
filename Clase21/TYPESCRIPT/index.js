9158e3cbdc5a4d7fd0292884920eb494514c2533
unknown <marcelobettini@yahoo.com.ar>
2022-06-07 20:20:38 -0300

    Ejemplo de consumo de API con cliente en Vanilla JS

---
+++ Clase21/TYPESCRIPT/index.js
@@ -0,0 +1,12 @@
+var hero = "Wonder Woman";
+console.log(hero);
+var inputEl = document.querySelector("input");
+inputEl.value = hero;
+var arr = ["plátano", "banana", "es the same"];
+var animal = "Giraffe";
+var age = 5;
+var isMammal = true;
+var circumference = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circumference(30));
---
+++ Clase21/TYPESCRIPT/index.ts
@@ -0,0 +1,16 @@
+const hero = "Wonder Woman"
+console.log(hero)
+const inputEl = document.querySelector("input")
+inputEl.value = hero
+const arr = ["plátano", "banana", "es the same"]
+
+
+let animal: string = "Giraffe"
+let age: number = 5
+let isMammal: boolean = true
+
+const circumference = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circumference(30))
